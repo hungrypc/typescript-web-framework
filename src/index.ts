859d381311c2d3f4becba919d213d5c92dcc045b
hpc <philipchanhk626@gmail.com>
2020-06-29 11:20:01 -0400

    fetching data from db with axios, User class fetch() function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
+// import axios from 'axios'
 import { User } from './models/User'
 
-const user = new User({ name: 'test', age: 26 })
 
-console.log(user.get('name'), user.get('age'))
-user.set({name: 'updated'})
-console.log(user.get('name'))
+const user = new User({ id: 1 })
 
-user.on('change', () => {
-  console.log('triggered')
-})
-user.on('change', () => {
-  console.log('triggered2')
-})
+user.fetch()
 
-console.log(user)
-user.trigger('change')
+
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 26
+// })
+
+// axios.get('http://localhost:3000/users/1')
+
+
+
+
+// const user = new User({ name: 'test', age: 26 })
+
+// console.log(user.get('name'), user.get('age'))
+// user.set({name: 'updated'})
+// console.log(user.get('name'))
+
+// user.on('change', () => {
+//   console.log('triggered')
+// })
+// user.on('change', () => {
+//   console.log('triggered2')
+// })
+
+// console.log(user)
+// user.trigger('change')
