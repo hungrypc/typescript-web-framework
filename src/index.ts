93693c3035a142ce9268388d2ea612484da8b231
hpc <philipchanhk626@gmail.com>
2020-06-29 10:30:34 -0400

    on fn in User class, adds to callback array for event

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { User } from './models/User'
 
 const user = new User({ name: 'test', age: 26 })
 
-console.log(user.get('name'))
-console.log(user.get('age'))
+console.log(user.get('name'), user.get('age'))
 user.set({name: 'updated'})
 console.log(user.get('name'))
+
+user.on('change', () => {
+  
+})
+
+console.log(user)
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void
+
 export class User {
+  events: { [key: string]: Callback[] } = {}
+
   constructor(private data: UserProps) {}
 
   get(propName: string): (number | string) {
@@ -14,4 +18,9 @@ export class User {
     Object.assign(this.data, update)
   }
 
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers
+  } 
 }
