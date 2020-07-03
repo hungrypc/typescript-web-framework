1a7c05b31b203764e7b28b5f8ac8060a83f79a4d
hpc <philipchanhk626@gmail.com>
2020-07-03 14:43:50 -0400

    showing how to buildUser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 // import axios from 'axios'
 import { User } from './models/User'
 
-const user = User.buildUser({})
+const user = User.buildUser({id: 1})
 
+user.on('change', () => {
+  console.log(user)
+})
+
+user.fetch()
 
 // const user = new User({ id: 1, name: 'newer name', age: 0 })
 
